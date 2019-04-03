7827da3e54c35e035531534dde90c94e75f59b2a
Tim Bastiaans <Tim_bastiaans@hotmail.nl>
2019-04-03 14:48:39 +0200

    portaitmode lock and ionic grid

--- morse-signal-app/src/app/app.component.ts
+++ morse-signal-app/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -12,8 +14,12 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private screenOrientation: ScreenOrientation
+
   ) {
+    // allow user rotate
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
     this.initializeApp();
   }
 
--- morse-signal-app/src/app/tab1/tab1.page.ts
+++ morse-signal-app/src/app/tab1/tab1.page.ts
@@ -118,12 +118,12 @@ export class Tab1Page implements OnInit {
             );
     }
 
-    getStorage() {
-        this.nativeStorage.setItem('theme', {themeName: 'night'})
-            .then(
-                () => this.theme.setTheme('night'),
-                error => console.error('Error storing theme', error)
-            );
+    historyList() {
+
+    }
+
+    scanningText() {
+
     }
 }
 
